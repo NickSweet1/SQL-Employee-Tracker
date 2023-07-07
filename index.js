b6d5b72debe101e3e363dc39258d8b9096bf03a0
Nick <nicksweetj@gmail.com>
2023-07-07 11:44:02 -0400

    added password.js file to hold the password for sql so it is not visible on git hub. also added to .gitignore file

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
+const sqlPassword = require('./password');
 
-// const connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: password,
-//     database: 'employees'
-//   });
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: sqlPassword,
+    database: 'employees'
+  });
 
 const inquirerPrompt = [
   {
@@ -16,7 +17,6 @@ const inquirerPrompt = [
     choices: [
       "View All Employees",
       "View All Roles",
-      "View All Employees",
       "Add a Department",
       "Add a Role",
       "Add an Employee",
@@ -27,7 +27,31 @@ const inquirerPrompt = [
 ];
 
 init = () => {
-  inquirer.prompt(inquirerPrompt);
+  inquirer.prompt(inquirerPrompt).then((answers) => {
+    switch (answers.start) {
+      case "View All Employees":
+        // insert logic
+        break;
+      case "View All Roles":
+        // insert logic
+        break;
+      case "Add a Department":
+        //insert logic
+        break;
+      case "Add a Role":
+        //insert logic
+        break;
+      case "Add an Employee":
+        //insert logic
+        break;
+      case "Update an Employee Role":
+        //insert logic
+        break;
+      case "Quit":
+        // connection.end() //Close the mysql connection
+        break;
+    }
+  });
 };
 
 init();
